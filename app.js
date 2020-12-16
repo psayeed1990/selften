b98fb59627ae62daf1d4ee3c22b4c97cd153234d
payeed1990 <sayeedmondal1412@gmail.com>
2020-12-16 21:50:42 +0600

    fix path

--- app.js
+++ app.js
@@ -53,6 +53,10 @@ app.use('/api', adminBalance);
 // set static folder
 app.use(express.static('client/build'));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
 const port = process.env.PORT || 4999;
 
 app.listen(port, () => {
